92ff74e1292f0beb365602d1ac064aa1f5b688f0
marcoAureliosm <49442587+marcoAureliosm@users.noreply.github.com>
2021-08-14 10:47:36 -0300

    Script de criação de pastas

---
+++ Create_folder.py
@@ -0,0 +1,14 @@
+import os
+
+
+diretorios = os.listdir(os.curdir)
+os.mkdir(f"./rede-megavia")
+os.mkdir(f"./rede-itech")
+os.mkdir(f"./rede-interativa-tsa")
+os.mkdir(f"./rede-interativa-pip")
+os.mkdir(f"./rede-infoweb")
+os.mkdir(f"./rede-fiberlink")
+os.mkdir(f"./rede-compunet")
+                    
+             
+		
